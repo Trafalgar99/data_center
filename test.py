2f87d6b95232ce9b8f4384e6fc48f1cbfaa2b644
Trafalgar99 <1286015267@qq.com>
2022-05-15 18:46:56 +0800

    ${date}

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+from cmath import log
+from http import client
 import json
+from socket import socket
+from statistics import harmonic_mean
+
+import django
 
 
 with open("./geodata.json", "r", encoding="utf8") as f:
@@ -10,3 +16,7 @@ print(data["35"])
 # service nginx restart
 # ps -aux | grep uwsgi
 # uwsgi --ini /root/mysite_uwsgi/mysite.ini
+
+def application(env, start_response):
+    start_response("200 ok", [("Content-Type", "text/html")])
+    return [b"Hello World"]
