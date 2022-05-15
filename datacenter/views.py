9f152cc35529ba3665810e45372af970789e0881
Trafalgar99 <1286015267@qq.com>
2022-05-15 14:43:48 +0800

    ${date}

--- datacenter/views.py
+++ datacenter/views.py
@@ -14,28 +14,9 @@ def z1(request):
     return render(request, 'z1.html', {})
 
 
-def z2(request):
-    return render(request, 'z2.html', {})
-
-
-def z3(request):
-    return render(request, 'z3.html', {})
-
-
-def y1(request):
-    return render(request, 'y1.html', {})
-
-
-def y2(request):
-    return render(request, 'y2.html', {})
-
-
-def y3(request):
-    return render(request, 'y3.html', {})
-
-
-def get_data(request, *args, **kwargs):
+def get_data(request):
     import json
+
     params = list(request.GET.keys())
     geoIndex = params[0]
     geoIndex = int(geoIndex)
